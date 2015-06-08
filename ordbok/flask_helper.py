2a3dcd2cb47fd9e73a48b2a7faa939ace2e7402a
Erik Taubeneck <erik.taubeneck@gmail.com>
2015-06-08 12:10:56 -0400

    add ordbok to app.extensions in flask helper

--- ordbok/flask_helper.py
+++ ordbok/flask_helper.py
@@ -8,6 +8,9 @@ class FlaskOrdbok(Ordbok):
         return super(FlaskOrdbok, self).__init__(**kwargs)
 
     def init_app(self, app):
+        if not hasattr(app, 'extensions'):
+            app.extensions = {}
+        app.extensions['ordbok'] = self
         self._root_path = app.config.root_path
 
     def app_run(self, app, *args, **kwargs):
